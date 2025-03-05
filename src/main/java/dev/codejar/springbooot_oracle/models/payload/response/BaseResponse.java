41499988bef10e0b24ce43ff369f61103bf2326f
fajar abdillah <fajardillah25@gmail.com>
2025-03-05 10:20:03 +0700

    add builder and allargconstr

--- src/main/java/dev/codejar/springbooot_oracle/models/payload/response/BaseResponse.java
+++ src/main/java/dev/codejar/springbooot_oracle/models/payload/response/BaseResponse.java
@@ -1,11 +1,15 @@
 package dev.codejar.springbooot_oracle.models.payload.response;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BaseResponse<T> {
 
     private Boolean success;
