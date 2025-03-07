964b2c30ec18cd37300468856d53d16b41585683
fajar abdillah <fajardillah25@gmail.com>
2025-03-07 23:09:05 +0700

    Update rename class name

--- src/main/java/dev/codejar/springbooot_oracle/models/payload/request/CsRequest.java
+++ src/main/java/dev/codejar/springbooot_oracle/models/payload/request/CsRequest.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Data
-public class CustomerRequest {
+public class CsRequest {
 
     @NotBlank(message = "id cannot be blank")
     private Long id;
